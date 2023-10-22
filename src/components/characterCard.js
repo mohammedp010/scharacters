263899601e7325f6eef336e47aea74e88251bb14
patra <patra@DESKTOP-NG8K2H1>
2023-10-22 18:09:31 +0530

    Fixed responsive content's bug

--- src/components/characterCard.js
+++ src/components/characterCard.js
@@ -1,41 +1,32 @@
 import React from 'react';
-import { Box,Stack, Heading, Text, Button, ButtonGroup, useColorModeValue } from '@chakra-ui/react';
+import {
+  SimpleGrid,
+  Box,
+  Text,
+  Heading,
+  Button
+} from '@chakra-ui/react';
 
 const CharacterCard = ({ character, onClick }) => {
-  const cardBgColor = useColorModeValue('white', 'gray.700');
 
   return (
-    <Box
-      borderWidth="10px"
-      borderRadius="3xl"
-      overflow="hidden"
-      cursor="pointer"
-      transition="transform 0.2s"
-      _hover={{ transform: 'scale(1.05)' }}
-      onClick={() => onClick(character)}
-      mb={4}
-      bg={cardBgColor}
-      ml="600px"
-      mr="-600px"
-      bgColor="black"
-      color="white"
-      w="250px"
-      h="300px" 
-    >
-    
-      <Stack mt='6' spacing='3' p={4}>
-        <Heading size='md'>{character.name}</Heading>
+    <SimpleGrid columns={2} spacing={4} minChildWidth="375px">
+      <Box
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+        cursor="pointer"
+        transition="transform 0.2s"
+        _hover={{ transform: 'scale(1.5)' }}
+        onClick={() => onClick(character)}
+        bg="gray"
+      >
+        <Heading size='md' mt="10">{character.name}</Heading>
         <Text>{character.height} meters</Text>
         <Text>{character.mass} kg</Text>
-      </Stack>
-      <Stack p={14}>
-        <ButtonGroup spacing='2'>
-          <Button variant='solid' ml="7" colorScheme='black'>
-            Tap!
-          </Button>
-        </ButtonGroup>
-      </Stack>
-    </Box>
+        <Button colorScheme='black' color="yellow">Tap!</Button>
+      </Box>
+    </SimpleGrid>
   );
 };
 
--- src/components/characterList.js
+++ src/components/characterList.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CharacterCard from './characterCard';
 import CharacterModal from './characterModal';
-import { Box, Heading, Flex, Spinner, Button, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Flex,
+  Spinner,
+  Button,
+  Text,
+  SimpleGrid,
+} from '@chakra-ui/react';
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
@@ -46,40 +54,44 @@ const CharacterList = () => {
   };
 
   return (
-    <Box className="character-list-render" width={['100%', '50%', '33.33%', '25%']} p={2}>
-      <Heading color="white" as="h1" size="xl" mb={4} ml="549" mr="-549">
+    <Box className="character-list-render" p={2}>
+      <Heading color="white" as="h1" size="xl" mb={4} ml="45" mr="-45">
         Star War Characters
       </Heading>
 
       {error && (
-        <Box mb={4} ml="549" mr="-549" >
+        <Box mb={4}>
           <Heading as='h2' size='2xl' color='red.500'>
-            Something Went Wrong!
+            Error
           </Heading>
           <Text color="red.500">{error}</Text>
         </Box>
       )}
 
-      {loading ? (
-        <Spinner size="xl" mb={4} ml="549" mr="-549" color="white" />
-      ) : (
-        <Flex className="character-list" flexWrap="wrap">
-          {characters.map((character) => (
+      <SimpleGrid columns={[1, 2, 3, 4]} spacing={4}>
+        {loading ? (
+          <Spinner size="xl" mb={4} ml="549" mr="-549" color="white" />
+        ) : (
+          characters.map((character) => (
             <CharacterCard
               key={character.name}
               character={character}
               onClick={() => handleCharacterClick(character)}
-              flexBasis={['100%', '48%', '32%', '24%']}
             />
-          ))}
-        </Flex>
-      )}
+          ))
+        )}
+      </SimpleGrid>
 
       <Flex justify="space-between" mt={4}>
-        <Button onClick={handlePrevPage} disabled={currentPage === 1} colorScheme="black" bgColor="grey" ml="549" mr="-549">
+        <Button
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          color="black"
+          bgColor="#f2ef2c"
+        >
           Previous
         </Button>
-        <Button onClick={handleNextPage} colorScheme="black" ml="549" mr="-549" bgColor="grey">
+        <Button onClick={handleNextPage} color="black" bgColor="#f2ef2c">
           Next
         </Button>
       </Flex>
